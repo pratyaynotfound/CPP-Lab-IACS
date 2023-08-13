0d2c086845afa0a3ee2ece7f67e6ed72ab3fa5ad
Pratyay Sarkar <104068902+pratyaynotfound@users.noreply.github.com>
2023-08-14 01:17:26 +0530

    Update Prob3_Pt2_evaluate.cpp

--- Assignment-1/Prob3_Pt2_evaluate.cpp
+++ Assignment-1/Prob3_Pt2_evaluate.cpp
@@ -2,12 +2,20 @@
 using namespace std;
 
 int main(){
+    //declare a variable
     double y;
+    
+    //take input
     cout<<"Enter a number:"<<endl;
     cin>>y;
+    
+    //evaluate the expression
     double x = y-20*6/9;//original
     double x_changed = y-20.0*6/9;
+    
+    //print the outputs
     cout<<"Result:"<<x<<endl;
     cout<<"Changed Result:"<<x_changed<<endl;
+    
     return 0;
 }
