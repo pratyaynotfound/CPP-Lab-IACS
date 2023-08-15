d43e35a2018583f2088e1a52f0a5731ebd4953d2
Pratyay Sarkar <pratyaysarkar2017@gmail.com>
2023-08-15 21:00:31 +0530

    Update Prob4-Bin_rep.cpp

--- Assignment-1/Prob4-Bin_rep.cpp
+++ Assignment-1/Prob4-Bin_rep.cpp
@@ -3,30 +3,28 @@
 using namespace std;
 
 //represent the number in binary format
-void conv_bin(int x,vector<int>* bin_rep){
-    for (int i = sizeof(x) * 8 - 1; i >= 0; --i) {
+void conv_bin(int x,vector<int>& bin_rep){
+    for (int i = sizeof(x) * 8-1; i >= 0; --i) {
         int bit = (x >> i) & 1;
-        (*bin_rep).push_back(bit);
+        bin_rep.push_back(bit);
     }
 }
 
 //check if the number is stored in 2's complement format
-bool check_2scomplement(int x,vector<int> original){
-    vector<int> complement;
+bool check_complement(int x, vector<int>& comp_str,vector<int>& bin_rep) {
     int carry = 1;
-    for (int i = sizeof(x) * 8 - 1; i >= 0; --i) {
-        int bit = ((x >> i) & 1)|1;
-        int sum = bit+carry;
-        complement[i] = sum % 2;
+    for (int i = 0; i<sizeof(x) * 8; i++) {
+        int sum = (bin_rep[i])-carry;
+        comp_str.push_back(sum % 2);
         carry = sum / 2;
-        complement.push_back(sum);
     }
-    bool flag = true;
-    for(int i:complement){
-        if(!(original[i]^complement[i]))
-            flag = false;
+    for(int j = 0;j<sizeof(x)*8;j++){
+        if(j==0)
+            continue;
+        if(comp_str[j]!=bin_rep[j])
+            return false;
     }
-    return flag;
+    return true;
 }
 
 int main(){
@@ -34,20 +32,23 @@ int main(){
     cout<<"Please enter a number:"<<endl;
     cin>>a;//take input
 
-    vector<int> arr;//initialize a vector for storing the binary representation 
-    conv_bin(a,&arr);//convert the number to binary
-
-    //print the binary representation in the console
-    for(int i : arr){
+    //convert to binary format
+    vector<int> bin_store;
+    conv_bin(a,bin_store);
+    cout<<"Stored in memory:"<<endl;
+    for(int i:bin_store){
         cout<<i;
     }
     cout<<endl;
 
-    //check whether it is stored in 2's complement format 
-    if(check_2scomplement)
-        cout<<"Yes, it is stored in 2's complement format!"<<endl;
-    else
-        cout<<"No, it is not stored in 2's complement format!"<<endl;
+    //check if it is stroed in 2's complement format
+    if(a<0){
+        vector<int> complement_str;
+        if(check_complement(a,complement_str,bin_store))
+            cout<<"It is stored in 2's complement format."<<endl;
+        else
+            cout<<"Not stored in 2's complement format."<<endl;
 
+    }
     return 0;
 }
