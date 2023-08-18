68dd331fe082f053ecb79a9aed04dd38b42213b4
Pratyay Sarkar <pratyaysarkar2017@gmail.com>
2023-08-18 22:39:48 +0530

    Update Round off code Problem no 5

--- Assignment-1/Prob5_Round-off.cpp
+++ Assignment-1/Prob5_Round-off.cpp
@@ -1,18 +1,17 @@
 #include<iostream>
 using namespace std;
 int main(){
-    float x ;
+    long double x;
     unsigned int k;
     //take inputs
     cout<<"Please enter a floating point number:"<<endl;
     cin>>x;
-
     cout<<"Enter a positive k: "<<endl;
     cin>>k;
 
     //rounding off
-    x = (int)(x*k+0.5);
-    x = x/k;
-    cout<<"Rounded off to the"<<k<<"th place: "<<x<<endl;
+    x = (long int)(x*k+0.5);
+    x= x/k;
+    printf("Rounded off to the %d th place: %Lf",k,x);
     return 0;
 }
