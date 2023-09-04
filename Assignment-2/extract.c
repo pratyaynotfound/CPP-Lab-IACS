47775321f23f6b7ee09f154c57075c268ce60e50
Pratyay Sarkar <pratyaysarkar2017@gmail.com>
2023-09-05 03:22:57 +0530

    add extract.c & message1.txt &message2.jpg remove extract.cpp

--- Assignment-2/extract.c
+++ Assignment-2/extract.c
@@ -1,9 +1,8 @@
-#include <iostream>
-#include <fstream>
-#include <cstdint>
-#include <arpa/inet.h>
-using namespace std;
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdint.h>
 
+//IP structure
 //     0                   1                   2                   3   
 //     0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 
 //    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
@@ -19,9 +18,9 @@ using namespace std;
 //    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 //    |                    Options                    |    Padding    |
 //    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-
-struct ip {
-    uint8_t hlenver;
+typedef struct ip_header {
+    uint8_t hlen:4;
+    uint8_t ver:4;
     uint8_t tos;
     uint16_t len;
     uint16_t id;
@@ -31,9 +30,7 @@ struct ip {
     uint16_t sum;
     uint32_t src;
     uint32_t dst;
-    uint32_t opd;
-};
-
+}ip_header;
 //     0                   1                   2                   3   
 //     0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 
 //    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
@@ -53,65 +50,62 @@ struct ip {
 //    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 //    |                             data                              |
 //    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-
-struct tcp {
-    uint16_t srcprt;
-    uint16_t destprt;
+typedef struct tcp_header {
+    uint16_t source;
+    uint16_t dest;
     uint32_t seq;
-    uint32_t ack;
-    uint32_t dta;
-    uint64_t off;
-    uint16_t chksum;
-    uint16_t urg;
-    uint32_t op_pad;
-    uint32_t data;
-};
+    uint32_t ack_seq;
+    uint8_t res1:4;
+    uint8_t doff:4;
+    uint8_t flags;
+    uint16_t window;
+    uint16_t check;
+    uint16_t urg_ptr;
+}tcp_header;
 
-
-#define IP_HLEN(lenver) ((lenver & 0x0f) * 4) // Multiply by 4 to get the header length in bytes
-
-int main(int argc, char* argv[]) {
+int main(int argc,char *argv[]){
+     //check if total inputs are not 3 
     if (argc != 3) {
-        cout << "Usage: extract <input_file> <output_file>" << endl;
+        printf("Usage: %s <input_file> <output_file>\n", argv[0]);
         return 1;
     }
 
-    ifstream inputFile(argv[1], ios::binary);
-    if (!inputFile.is_open()) {
-        cerr << "Error opening input file." << endl;
-        return 2;
+    //open files
+    FILE *inputFile = fopen(argv[1],"rb");
+    FILE *outputFile = fopen(argv[2],"wb");
+    if (inputFile == NULL || outputFile == NULL) {
+        perror("Error opening file");
+        exit(0);
     }
+    
+    //seek to the end of the file
+    fseek(inputFile,0,SEEK_END);
+    long size = ftell(inputFile);
+    fseek(inputFile,0,SEEK_SET);
 
-    ofstream outputFile(argv[2]);
-    if (!outputFile.is_open()) {
-        cerr << "Error opening output file." << endl;
-        inputFile.close();
-        return 3;
+    // printf("%ld",size); //debug
+
+    //declare a buffer for storing the data
+    uint8_t *buffer = malloc(size);
+    fread(buffer, size, 1, inputFile);//read the data
+    
+    //Ip TCP headers
+    ip_header *ip;
+    tcp_header *tcp;
+
+    //fo through all the packets and check for data 
+    long i = 0;
+    while(i<size){
+        ip = (ip_header *)(buffer+i);
+        i = i+ip->hlen*4;
+        tcp = (tcp_header *)(buffer+i);
+        i = i+tcp->doff*4;
+
+        uint16_t length = ntohs(ip->len)-(ip->hlen*4+tcp->doff*4);
+        if(length>0){
+            fwrite(buffer+i,1,length,outputFile);
+        }
+        i = i+length;
     }
-
-    struct ip packet;
-    inputFile.read(reinterpret_cast<char*>(&packet), sizeof(ip));
-
-    int datasize = ntohs(packet.len) - IP_HLEN(packet.hlenver);
-
-
-    std::vector<uint8_t> buffer(1);
-    int padding = 
-    inputFile.seekg(packet.hlenver, std::ios::cur);
-    int i = -1;
-    while (inputFile.read(reinterpret_cast<char*>(buffer.data()), buffer.size())) {
-        uint8_t byte = buffer[0];
-        char character = static_cast<char>(byte);
-        i++;
-        if(i<packet.hlenver)
-            continue;
-        outputFile<<character;
-        cout<<character;
-    }
-    cout<<endl;
-
-    inputFile.close();
-    outputFile.close();
-
-    return 0;
+    fclose(outputFile);
 }
