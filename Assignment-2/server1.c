3a49f7a19a450b0b6f938c17d8d859381dcc80bb
Pratyay Sarkar <pratyaysarkar2017@gmail.com>
2023-09-04 23:25:45 +0530

    update server1.c

--- Assignment-2/server1.c
+++ Assignment-2/server1.c
@@ -10,68 +10,77 @@
 #define MAXOPEN 5
 #define BUFSIZE 1024
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
 
   int listenfd, connfd;
   FILE *fp;
   struct sockaddr_in server;
-  
-  if (argc != 3) {
+
+  if (argc != 3)
+  {
     puts("Usage: server <port> <file>");
     return 1;
   }
- 
-  if ((fp=fopen(argv[2],"rb")) == 0) {
+
+  if ((fp = fopen(argv[2], "rb")) == 0)
+  {
     perror("Cannot find file to serve");
     return 2;
   }
 
-  memset(&server,0,sizeof(server));
+  memset(&server, 0, sizeof(server));
   server.sin_family = AF_INET;
   server.sin_addr.s_addr = htonl(INADDR_ANY);
   server.sin_port = htons(atoi(argv[1]));
-  
-  if ((listenfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
+
+  if ((listenfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
+  {
     perror("Cannot create server socket");
     return 3;
   }
 
-  if (bind(listenfd, (struct sockaddr *) &server, sizeof(server)) < 0) {
+  if (bind(listenfd, (struct sockaddr *)&server, sizeof(server)) < 0)
+  {
     perror("Cannot open the interface");
     return 4;
   }
 
-  if (listen(listenfd,MAXOPEN) < 0) {
+  if (listen(listenfd, MAXOPEN) < 0)
+  {
     perror("Cannot listen on the interface");
     return 5;
   }
 
-  for(;;) {
+  for (;;)
+  {
 
-    if ( (connfd = accept(listenfd, (struct sockaddr *) NULL, NULL)) < 0 ) {
+    if ((connfd = accept(listenfd, (struct sockaddr *)NULL, NULL)) < 0)
+    {
       perror("Error accepting a client connection");
       return 6;
     }
 
-    while(!feof(fp)) {
+    while (!feof(fp))
+    {
       char bytes[BUFSIZE];
-      int r,w;
+      int r, w;
 
-      r = fread(bytes,sizeof(char),BUFSIZE,fp);
+      r = fread(bytes, sizeof(char), BUFSIZE, fp);
 
-      while(w<r) {
-	int total = write(connfd,bytes,r);
-	if (total < 0) {
-	  perror("Error writing data to client");
-	  return 7;
-	}
-	w+=total;
+      while (w < r)
+      {
+        int total = write(connfd, bytes, r);
+        if (total < 0)
+        {
+          perror("Error writing data to client");
+          return 7;
+        }
+        w += total;
       }
     }
-    fseek(fp,0,SEEK_SET);
-    
+    fseek(fp, 0, SEEK_SET);
     close(connfd);
-
-    return 0;
-}
+  }
+  return 0;
 }
