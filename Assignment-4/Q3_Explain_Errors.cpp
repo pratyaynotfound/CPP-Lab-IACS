91d2b4931adc8d928a0e4ea2b6bd4833f0316ac6
Pratyay Sarkar <pratyaysarkar2017@gmail.com>
2023-09-13 13:02:46 +0530

    Update Q3

--- Assignment-4/Q3_Explain_Errors.cpp
+++ Assignment-4/Q3_Explain_Errors.cpp
@@ -2,9 +2,16 @@
 const char* createString(){
     return "Practice makes a man perfect";
 }
+/*
+In this program we are returning the pointer of a variable that is local to  the 'createInt' function. We know that c program stores the local variables in the stack memory and they get deleted after goes out of the scope. 
 int* createInt(){
     int x = 100;
     return &x;
+}
+*/
+int* createInt(){
+    int* x = new int(100); 
+    return x;
 }
 int main(){
     const char *str = createString();
@@ -12,6 +19,3 @@ int main(){
     int *ip = createInt();
     std::cout << "integer = " << *ip << std::endl;
 }
-/*
-In this program we are returning the pointer of a variable that is local to  the 'createInt' function. We know that c program stores the local variables in the stack memory and they get deleted after goes out of the scope. 
-*/
